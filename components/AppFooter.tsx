5db770d8308fdcd4e11ba5c62bd0b8d619a39bec
Aim4price.com <aim4price@gmail.com>
2026-04-11 17:00:46 +0200

    Update AppFooter.tsx

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -23,6 +23,7 @@ export default function AppFooter() {
               <Link href="/valuation">Valuation</Link>
               <Link href="/asset-register">Asset Register</Link>
               <Link href="/marketplace">Marketplace</Link>
+              <Link href="/account">Account</Link>
             </div>
 
             <div className={styles.linkColumn}>
@@ -37,7 +38,7 @@ export default function AppFooter() {
       <div className={styles.metaRow}>
         <div className={styles.shell}>
           <span>© {year} Aim4price</span>
-          <span>Built for clearer machinery decisions.</span>
+          <span>Indicative valuations should be confirmed for formal insurance, finance, or transactional use.</span>
         </div>
       </div>
     </footer>
