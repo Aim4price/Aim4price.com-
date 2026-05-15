9822cf8b620b9470ab97589885fbff1da1b0ac26
Aim4price.com <aim4price@gmail.com>
2026-05-15 11:48:00 +0200

    Add files via upload

--- leads/page.tsx
+++ leads/page.tsx
@@ -1,15 +1,1 @@
-import { redirect } from 'next/navigation';
-import { getServerSession } from '../../lib/auth-session';
-import LeadsClient from './leads-client';
-
-export const runtime = 'nodejs';
-
-export default async function LeadsPage() {
-  const session = await getServerSession();
-
-  if (!session) {
-    redirect('/auth#signup');
-  }
-
-  return <LeadsClient />;
-}
+export { default, runtime } from '../app/leads/page';
