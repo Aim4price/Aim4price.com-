882404db50c5fe49ca06125d3957513aaf67cd8f
Aim4price.com <aim4price@gmail.com>
2026-07-05 01:35:57 +0200

    Add files via upload

--- app/fuel/page.tsx
+++ app/fuel/page.tsx
@@ -1,11 +1,23 @@
+import { redirect } from "next/navigation";
 import { requireActivePageAccess } from "../../lib/account-access";
+import { getAccountProfile } from "../../lib/account-profile";
 import FuelClient from "./fuel-client";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export default async function FuelPage() {
-  await requireActivePageAccess();
+  const { session } = await requireActivePageAccess();
+
+  const profile = await getAccountProfile({
+    id: session.user.id,
+    name: session.user.name,
+    email: session.user.email,
+  });
+
+  if (profile.accountType !== "owner") {
+    redirect("/account");
+  }
 
   return <FuelClient />;
 }
