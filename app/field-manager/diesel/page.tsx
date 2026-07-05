f7d7fa3c2dfd528c63c2c919c3e0140269b64f63
Aim4price.com <aim4price@gmail.com>
2026-07-05 21:24:03 +0200

    Add files via upload

--- app/field-manager/diesel/page.tsx
+++ app/field-manager/diesel/page.tsx
@@ -1,1 +1,8 @@
+import FieldManagerDieselClient from '../field-manager-diesel-client';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default function FieldManagerDieselPage() {
+  return <FieldManagerDieselClient />;
+}
