d22b981539b6ceb68371a4cd9aeba8a4d4ef0f05
Aim4price.com <aim4price@gmail.com>
2026-07-05 18:23:57 +0200

    Add files via upload

--- app/field-manager/login/page.tsx
+++ app/field-manager/login/page.tsx
@@ -1,1 +1,8 @@
+import FieldManagerLoginClient from '../field-manager-login-client';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default function FieldManagerLoginPage() {
+  return <FieldManagerLoginClient />;
+}
