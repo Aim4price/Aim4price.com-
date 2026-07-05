d4c0932784fde3f981f296827040f65eff5e0576
Aim4price.com <aim4price@gmail.com>
2026-07-05 18:23:25 +0200

    Add files via upload

--- app/field-manager/assets/page.tsx
+++ app/field-manager/assets/page.tsx
@@ -1,1 +1,8 @@
+import FieldManagerAssetsClient from '../field-manager-assets-client';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default function FieldManagerAssetsPage() {
+  return <FieldManagerAssetsClient />;
+}
