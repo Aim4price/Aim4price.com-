4581b8f9b7546d93ae71d5e5fe50cb82b7b973ae
Aim4price.com <aim4price@gmail.com>
2026-07-05 21:26:02 +0200

    Add files via upload

--- app/field-manager/assets/[publicAssetCode]/page.tsx
+++ app/field-manager/assets/[publicAssetCode]/page.tsx
@@ -1,1 +1,14 @@
+import ScanClient from '../../../scan/[publicAssetCode]/scan-client';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+type PageProps = {
+  params: {
+    publicAssetCode: string;
+  };
+};
+
+export default function FieldManagerAssetUpdatePage({ params }: PageProps) {
+  return <ScanClient publicAssetCode={params.publicAssetCode ?? ''} fieldManagerMode />;
+}
