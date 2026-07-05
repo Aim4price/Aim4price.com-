8c5bf86375f9233608f6a0bfe6348c3571e99ad6
Aim4price.com <aim4price@gmail.com>
2026-07-05 21:25:04 +0200

    Add files via upload

--- app/field-manager/diesel/[publicFuelStorageCode]/page.tsx
+++ app/field-manager/diesel/[publicFuelStorageCode]/page.tsx
@@ -1,1 +1,14 @@
+import FuelScanClient from '../../../fuel-scan/[publicFuelStorageCode]/fuel-scan-client';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+type PageProps = {
+  params: {
+    publicFuelStorageCode: string;
+  };
+};
+
+export default function FieldManagerDieselUpdatePage({ params }: PageProps) {
+  return <FuelScanClient publicFuelStorageCode={params.publicFuelStorageCode ?? ''} fieldManagerMode />;
+}
