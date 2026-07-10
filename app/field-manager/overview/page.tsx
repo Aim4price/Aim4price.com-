f142888dc7cfcf91145c2ac07a1a25103f1ec8e6
Aim4price.com <aim4price@gmail.com>
2026-07-10 23:45:00 +0200

    Add files via upload

--- app/field-manager/overview/page.tsx
+++ app/field-manager/overview/page.tsx
@@ -1,1 +1,8 @@
+import FieldManagerOverviewClient from '../field-manager-overview-client';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default function FieldManagerOverviewPage() {
+  return <FieldManagerOverviewClient />;
+}
