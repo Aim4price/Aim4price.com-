296d87ed447bf5b4c67151957d07e1f18da886a8
Aim4price.com <aim4price@gmail.com>
2026-07-14 19:01:53 +0200

    Add files via upload

--- app/app/valuation/page.tsx
+++ app/app/valuation/page.tsx
@@ -1,1 +1,18 @@
+import dealerStyles from '../../dealer/dealer.module.css';
+import ValuationClient from '../../valuation/valuation-client';
+import { requireOwnerAppPageAccess } from '../../../lib/owner-app-access';
+import OwnerAppNav from '../owner-app-nav';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default async function OwnerAppValuationPage() {
+  await requireOwnerAppPageAccess();
+
+  return (
+    <div className={dealerStyles.module}>
+      <OwnerAppNav title="Get Estimate" backHref="/app" />
+      <ValuationClient dealerAppMode appHomeHref="/app" marketplaceHref="/marketplace" />
+    </div>
+  );
+}
