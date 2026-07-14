26735e973d275c492ecc4d0b88e4b4f83eb6b6b6
Aim4price.com <aim4price@gmail.com>
2026-07-15 00:03:47 +0200

    Add files via upload

--- app/owner-app/marketplace/page.tsx
+++ app/owner-app/marketplace/page.tsx
@@ -1,1 +1,22 @@
+import MarketplaceClient from '../../marketplace/marketplace-client';
+import { requireOwnerAppPageAccess } from '../../../lib/owner-app-access';
+import OwnerAppNav from '../owner-app-nav';
+import styles from '../owner-app.module.css';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default async function OwnerAppMarketplacePage() {
+  await requireOwnerAppPageAccess();
+  return (
+    <div className={styles.module}>
+      <OwnerAppNav />
+      <MarketplaceClient
+        isSignedIn
+        accountType="owner"
+        ownerAppMode
+        initialFilters={{ brand: '', model: '', drive: '', type: '' }}
+      />
+    </div>
+  );
+}
