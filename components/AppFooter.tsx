bc647c59cd366d4cd8c7f3520ebc8fc1db458a61
Aim4price.com <aim4price@gmail.com>
2026-07-14 21:28:58 +0200

    Add files via upload

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -67,7 +67,6 @@ export default function AppFooter() {
     pathname?.startsWith('/field-manager') ||
     pathname?.startsWith('/scan') ||
     pathname?.startsWith('/dealer') ||
-    pathname?.startsWith('/app') ||
     pathname?.startsWith('/account/dealer-app')
   ) {
     return null;
