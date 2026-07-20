20e29083841f82289b7e3deba8eaf4c7b4ac778c
Aim4price.com <aim4price@gmail.com>
2026-07-20 08:11:35 +0200

    Add files via upload

--- app/field-manager/overview/page.tsx
+++ app/field-manager/overview/page.tsx
@@ -3,16 +3,6 @@ import FieldManagerOverviewClient from '../field-manager-overview-client';
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
-type PageProps = {
-  searchParams?: {
-    range?: string;
-  };
-};
-
-export default function FieldManagerOverviewPage({ searchParams }: PageProps) {
-  return (
-    <FieldManagerOverviewClient
-      initialRange={searchParams?.range === 'week' ? 'week' : 'upcoming'}
-    />
-  );
+export default function FieldManagerOverviewPage() {
+  return <FieldManagerOverviewClient />;
 }
