8a655e22219794f94c43a51b242cd7bc5ddafb3d
Aim4price.com <aim4price@gmail.com>
2026-07-25 03:04:19 +0200

    Add files via upload

--- app/marketplace/browse/page.tsx
+++ app/marketplace/browse/page.tsx
@@ -1,1 +1,5 @@
+export { generateMetadata } from '../marketplace-browse-page';
+export { default } from '../marketplace-browse-page';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
