ea36e951cd1a35db654a72e53a7e3f06c18890a3
Aim4price.com <aim4price@gmail.com>
2026-07-29 19:38:09 +0200

    Add files via upload

--- app/dealer/discovery/page.tsx
+++ app/dealer/discovery/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from 'next/navigation';
 import { getServerSession } from '../../../lib/auth-session';
 import { getAccountProfile } from '../../../lib/account-profile';
 import AssetDiscoveryClient from '../../asset-discovery/asset-discovery-client';
-import DealerNav from '../dealer-nav';
 import styles from '../dealer.module.css';
 
 export const runtime = 'nodejs';
@@ -21,7 +20,6 @@ export default async function DealerDiscoveryPage() {
 
   return (
     <div className={styles.module}>
-      <DealerNav />
       <main>
         <AssetDiscoveryClient dealerAppMode />
       </main>
