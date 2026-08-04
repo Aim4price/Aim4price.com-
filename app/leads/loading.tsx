da3469458f60afcde355b0010350a15ea8e677e6
Aim4price.com <aim4price@gmail.com>
2026-08-04 09:53:34 +0200

    Add responsive loading and performance guards

---
+++ app/leads/loading.tsx
@@ -0,0 +1,10 @@
+import PageLoadingState from '../../components/PageLoadingState';
+
+export default function LeadsLoading() {
+  return (
+    <PageLoadingState
+      label="Loading My Leads"
+      detail="Getting your latest client requests ready."
+    />
+  );
+}
