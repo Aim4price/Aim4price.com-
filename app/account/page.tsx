b38ead3e6fa4a9305fbd517fc21b4da0128099cb
Aim4price.com <aim4price@gmail.com>
2026-08-04 09:00:43 +0200

    Load account hub data on the server

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,4 @@
+import { getAccountProfile, getAccountScanPinStatus } from "../../lib/account-profile";
 import { requireActivePageAccess } from "../../lib/account-access";
 import AccountClient from "./account-client";
 
@@ -5,7 +6,15 @@ export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export default async function AccountPage() {
-  await requireActivePageAccess();
+  const { session } = await requireActivePageAccess();
+  const [profile, scanPinStatus] = await Promise.all([
+    getAccountProfile({
+      id: session.user.id,
+      name: session.user.name,
+      email: session.user.email,
+    }),
+    getAccountScanPinStatus(session.user.id),
+  ]);
 
-  return <AccountClient />;
+  return <AccountClient initialProfile={profile} initialScanPinStatus={scanPinStatus} />;
 }
