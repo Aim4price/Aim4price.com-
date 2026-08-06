004353bd0950f7462d65bdcc51ccd2c379eead2c
Aim4price.com <aim4price@gmail.com>
2026-08-06 19:54:50 +0200

    Mirror Owner flow in Field Manager: add app/field-manager/diesel/storage/page.tsx

---
+++ app/field-manager/diesel/storage/page.tsx
@@ -0,0 +1,8 @@
+import FieldManagerDieselClient from '../../field-manager-diesel-client';
+
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export default function FieldManagerFuelStoragePage() {
+  return <FieldManagerDieselClient />;
+}
