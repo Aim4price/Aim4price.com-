c7d0eec1380350703997fd38e5fe12078bf308b8
Aim4price.com <aim4price@gmail.com>
2026-08-16 01:07:36 +0200

    Add showroom location to advert preview

--- components/AdStudioClient.tsx
+++ components/AdStudioClient.tsx
@@ -271,6 +271,7 @@ export default function AdStudioClient({ dealerAppMode = false, middlemanMode = 
     sellerName: draft.contactName || 'Sales contact',
     sellerPhone: draft.phone || '082 000 0000',
     sellerCompany: previewName,
+    location: 'George, Western Cape',
     imageUrls: [],
     brand: {
       name: draft.name,
