c0638136f6fb8425b13d98dbc321d02c02a531e8
Aim4price.com <aim4price@gmail.com>
2026-09-02 03:16:40 +0200

    Read the homepage intro session state

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import { redirectAdminToAdmin } from '../lib/account-access';
 import AppHeader from '../components/AppHeader';
 import HomeHeroExperience from './home-hero-experience';
@@ -7,14 +8,17 @@ import styles from './page.module.css';
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
+const HOME_INTRO_COOKIE = 'a4p_home_intro';
+
 export default async function HomePage() {
   await redirectAdminToAdmin();
+  const showHomeIntro = cookies().get(HOME_INTRO_COOKIE)?.value !== 'v1';
 
   return (
     <main className={styles.page}>
       <AppHeader active="home" brandAlignment="working-column" />
 
-      <HomeHeroExperience />
+      <HomeHeroExperience showIntro={showHomeIntro} />
 
       <HomeRoleSelector />
     </main>
