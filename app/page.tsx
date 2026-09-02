c7ce2209c4418b616388b2a45948e22a6d50e5c5
Aim4price.com <aim4price@gmail.com>
2026-09-02 03:25:14 +0200

    Revert app/page.tsx to PR #534

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { cookies } from 'next/headers';
 import { redirectAdminToAdmin } from '../lib/account-access';
 import AppHeader from '../components/AppHeader';
 import HomeHeroExperience from './home-hero-experience';
@@ -8,17 +7,14 @@ import styles from './page.module.css';
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
-const HOME_INTRO_COOKIE = 'a4p_home_intro';
-
 export default async function HomePage() {
   await redirectAdminToAdmin();
-  const showHomeIntro = cookies().get(HOME_INTRO_COOKIE)?.value !== 'v1';
 
   return (
     <main className={styles.page}>
       <AppHeader active="home" brandAlignment="working-column" />
 
-      <HomeHeroExperience showIntro={showHomeIntro} />
+      <HomeHeroExperience />
 
       <HomeRoleSelector />
     </main>
