395631d31a24fab5e38c7167bd48d09685c3b7c7
Aim4price.com <aim4price@gmail.com>
2026-09-02 16:17:36 +0200

    Restore the Home page to the shared responsive shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { redirectAdminToAdmin } from '../lib/account-access';
 import AppHeader from '../components/AppHeader';
-import HomeDisplayCheck from './home-display-check';
 import HomeHeroExperience from './home-hero-experience';
 import HomeRoleSelector from './home-role-selector';
+import styles from './page.module.css';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
@@ -11,16 +11,13 @@ export default async function HomePage() {
   await redirectAdminToAdmin();
 
   return (
-    <HomeDisplayCheck>
-      <AppHeader
-        active="home"
-        brandAlignment="working-column"
-        standardCanvas
-      />
+    <main className={styles.page}>
+      <AppHeader active="home" brandAlignment="working-column" />
 
       <HomeHeroExperience />
 
       <HomeRoleSelector />
-    </HomeDisplayCheck>
+    </main>
   );
 }
+
