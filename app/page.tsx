73adb54d6e834a4653decb02a80bee87b5902a86
Aim4price.com <aim4price@gmail.com>
2026-09-02 10:05:35 +0200

    Gate the homepage through display setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { redirectAdminToAdmin } from '../lib/account-access';
 import AppHeader from '../components/AppHeader';
+import HomeDisplayCheck from './home-display-check';
 import HomeHeroExperience from './home-hero-experience';
 import HomeRoleSelector from './home-role-selector';
-import styles from './page.module.css';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
@@ -11,12 +11,12 @@ export default async function HomePage() {
   await redirectAdminToAdmin();
 
   return (
-    <main className={styles.page}>
+    <HomeDisplayCheck>
       <AppHeader active="home" brandAlignment="working-column" />
 
       <HomeHeroExperience />
 
       <HomeRoleSelector />
-    </main>
+    </HomeDisplayCheck>
   );
 }
